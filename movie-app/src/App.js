ea3723afac769ccbd82c4d8e60c21cf9165bfd62
Sinyoung3016 <tasha0415@naver.com>
2021-01-11 02:54:29 +0900

    Update Movie App : Make Form of Screen

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,20 +1,46 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css"
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    isLoading : true,
-    movies : []
+    isLoading: true,
+    movies: []
   };
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    this.setState({ movies: movies, isLoading: false });
+  }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading : false});
-    }, 6000);
-    //mount 6초 후, isLoading 값은 false
-  };
-  render(){
-    const { isLoading } = this.state;
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
     return (
-    <div>{ isLoading ? "Loading" : "We are ready"}</div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading</span>
+          </div>
+        ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  url={movie.url}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary.slice(0,140) + "..."}
+                  poster={movie.medium_cover_image}
+                  genres = {movie.genres}
+                />
+              ))}
+            </div>
+          )
+        }
+      </section>
     );
   };
 }
